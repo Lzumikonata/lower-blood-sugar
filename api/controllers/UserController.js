d03252cb0c2f66df4695b30d6cbb3bd21db2fb92
WittBulter <nanazuimeng123@gmail.com>
2017-01-12 18:32:50 +0800

    创建用户接口

---
+++ api/controllers/UserController.js
@@ -0,0 +1,30 @@
+/**
+ * Created by WittBulter on 2017/1/12.
+ */
+
+module.exports = {
+
+	show: (req, res) =>{
+
+	},
+
+	create: (req, res) =>{
+
+	},
+
+	update: (req, res) =>{
+
+	},
+
+	destroy: (req, res) =>{
+
+	},
+
+	createForAdmin: (req, res) =>{
+
+	},
+
+	updateForAdmin: (req, res) =>{
+
+	}
+}
